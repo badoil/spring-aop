92be43303f9bb218ae65e9e56c8c0b44ab3ba10a
oneal.oh <oneal.oh@kakaomobility.com>
2022-10-01 13:50:13 +0900

    update: WithinTest 추가

--- src/test/java/hello/aop/pointcut/WithinTest.java
+++ src/test/java/hello/aop/pointcut/WithinTest.java
@@ -2,6 +2,7 @@ package hello.aop.pointcut;
 
 import hello.aop.member.MemberServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.aspectj.AspectJExpressionPointcut;
 
@@ -34,5 +35,18 @@ public class WithinTest {
         pointcut.setExpression("within(hello.aop..*)");
         assertThat(pointcut.matches(helloMethod,
                 MemberServiceImpl.class)).isTrue();
+    }
+
+    @Test
+    @DisplayName("타켓의 타입에만 직접 적용, 인터페이스를 선정하면 안된다.")
+    void withinSuperTypeFalse() {
+        pointcut.setExpression("within(hello.aop.member.MemberService)");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isFalse();
+    }
+    @Test
+    @DisplayName("execution은 타입 기반, 인터페이스를 선정 가능.")
+    void executionSuperTypeTrue() {
+        pointcut.setExpression("execution(* hello.aop.member.MemberService.*(..))");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
     }
 }
